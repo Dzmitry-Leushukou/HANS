7bc588f3c05f54cfa9ce73d0955dd5eefcc6bea4
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-08-13 23:52:44 +0300

    First

---
+++ Source/mainmenu.cpp
@@ -0,0 +1,14 @@
+#include "mainmenu.h"
+#include "ui_mainmenu.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
